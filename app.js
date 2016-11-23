353b38466c0d80c597e0f021060ac1c389e3edbb
Bang Nguyen <YOUR EMAIL ADDRESS>
2016-11-23 21:48:40 +0200

    Setup the lock dynamically

--- app.js
+++ app.js
@@ -1,139 +1,146 @@
-
 $(document).ready(function () {
 
+	var clientID = "";
+	var domain = "";
+	$.getJSON("http://www.balticapp.fi/lukeA/authzero", function (result) {
+		clientID = result.AUTH0_CLIENT_ID;
+		domain = result.AUTH0_DOMAIN;
 
-	//	$.getJSON("http://www.balticapp.fi/lukeA/authzero", function (result) {
-
-	var lock = new Auth0Lock(AUTH0_CLIENT_ID, AUTH0_DOMAIN, {
-		auth: {
-			//			redirectUrl: 'http://balticapp.fi/lukeA/callback',
-			responseType: 'token',
-			params: {
-				scope: 'openid email'
-			} //Details: https://auth0.com/docs/scopes
-		}
-	});
-
-
-	$('.btn-login').click(function (e) {
-		e.preventDefault();
-		lock.show();
-	});
-
-	$('.btn-logout').click(function (e) {
-		e.preventDefault();
-		logout();
-	})
-
-	lock.on("authenticated", function (authResult) {
-		lock.getProfile(authResult.idToken, function (error, profile) {
-			if (error) {
-				// Handle error
-				return;
+		var lock = new Auth0Lock(clientID, domain, {
+			auth: {
+				responseType: 'token',
+				params: {
+					scope: 'openid email'
+				} //Details: https://auth0.com/docs/scopes
 			}
-//			console.log('Authorization: Bearer ' + authResult.idToken);
-//			console.log('acstoken: ' + authResult.accessToken);
-//			console.log('payload: ' + authResult.idTokenPayload);
-//			console.log('profile: ' + JSON.stringify(profile));
-
-			localStorage.setItem('id_token', authResult.idToken);
-			localStorage.setItem('acstoken', authResult.accessToken);
-			localStorage.setItem('payload', authResult.idTokenPayload);
-			//			localStorage.setItem('state', authResult.state);
-			localStorage.setItem('profile', JSON.stringify(profile));
-
-			// Display user information
-			show_profile_info(profile);
 		});
 
-	});
 
-	$.ajaxSetup({
-		'beforeSend': function (xhr) {
-			if (localStorage.getItem('id_token')) {
-				xhr.setRequestHeader('Authorization',
-					'Bearer ' + localStorage.getItem('id_token'));
-			}
-			if (localStorage.getItem('acstoken')) {
-				xhr.setRequestHeader('acstoken',
-					localStorage.getItem('acstoken'));
-			}
-		}
-	});
-
-	var accessToken = localStorage.getItem('acstoken');
-	var idToken = localStorage.getItem('id_token');
-
-	function httpGetAsync(theUrl, callback) {
-		var xmlHttp = new XMLHttpRequest();
-		xmlHttp.onreadystatechange = function () {
-			if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-				callback(xmlHttp.responseText);
-		}
-		xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-		xmlHttp.send(null);
-	}
-
-
-	httpGetAsync('http://www.balticapp.fi/lukeA/login', function (response) {
-		response.text().then(function (t) {
-			if (response.status !== 200) {
-				console.log('error');
-				return;
-			}
-			alert('API Response: ' + JSON.stringify(JSON.parse(t)));
-		}).catch(function (err) {
-			alert('error: ' + err);
+		$('.btn-login').click(function (e) {
+			e.preventDefault();
+			lock.show();
 		});
-	});
 
+		$('.btn-logout').click(function (e) {
+			e.preventDefault();
+			logout();
+		})
 
-	//	fetch('http://www.balticapp.fi/lukeA/login', {
-	//		method: 'get',
-	//		headers: new Headers({
-	//			'Content-Type': 'application/json',
-	//			'Authorization': 'Bearer ' + idToken,
-	//			'acstoken': accessToken
-	//		})
-	//	}).then(function (response) {
-	//		response.text().then(function (t) {
-	//			if (response.status !== 200) {
-	//				console.log('error');
-	//				return;
-	//			}
-	//			alert('API Response: ' + JSON.stringify(JSON.parse(t)));
-	//		})
-	//	}).catch(function (err) {
-	//		alert('error: ' + err);
-	//	});
-
-	//retrieve the profile:
-	var retrieve_profile = function () {
-		var id_token = localStorage.getItem('id_token');
-		if (id_token) {
-			lock.getProfile(id_token, function (err, profile) {
-				if (err) {
-					return alert('There was an error getting the profile: ' + err.message);
+		lock.on("authenticated", function (authResult) {
+			lock.getProfile(authResult.idToken, function (error, profile) {
+				if (error) {
+					// Handle error
+					return;
 				}
+				//			console.log('Authorization: Bearer ' + authResult.idToken);
+				//			console.log('acstoken: ' + authResult.accessToken);
+				//			console.log('payload: ' + authResult.idTokenPayload);
+				//			console.log('profile: ' + JSON.stringify(profile));
+
+				localStorage.setItem('id_token', authResult.idToken);
+				localStorage.setItem('acstoken', authResult.accessToken);
+				localStorage.setItem('payload', authResult.idTokenPayload);
+				//			localStorage.setItem('state', authResult.state);
+				localStorage.setItem('profile', JSON.stringify(profile));
+
 				// Display user information
 				show_profile_info(profile);
 			});
+
+		});
+
+		$.ajaxSetup({
+			'beforeSend': function (xhr) {
+				if (localStorage.getItem('id_token')) {
+					xhr.setRequestHeader('Authorization',
+						'Bearer ' + localStorage.getItem('id_token'));
+				}
+				if (localStorage.getItem('acstoken')) {
+					xhr.setRequestHeader('acstoken',
+						localStorage.getItem('acstoken'));
+				}
+			}
+		});
+
+		var accessToken = localStorage.getItem('acstoken');
+		var idToken = localStorage.getItem('id_token');
+
+		function httpGetAsync(theUrl, callback) {
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.onreadystatechange = function () {
+				if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+					callback(xmlHttp.responseText);
+			}
+			xmlHttp.open("GET", theUrl, true);
+			xmlHttp.setRequestHeader("Authorization", "Bearer " + localStorage.getItem('id_token'));
+			xmlHttp.setRequestHeader("acstoken", localStorage.getItem('acstoken')); // true for asynchronous 
+			xmlHttp.send(null);
 		}
-	};
-
-	var show_profile_info = function (profile) {
-		$('.nickname').text(profile.nickname);
-		$('.btn-login').hide();
-		$('.avatar').attr('src', profile.picture).show();
-		$('.btn-logout').show();
-	};
 
 
-	var logout = function () {
-		localStorage.removeItem('id_token');
-		window.location.href = "/";
-	};
+		httpGetAsync('http://www.balticapp.fi/lukeA/login', function (response) {
+			console.log(response);
 
-	retrieve_profile();
-	//	});
+			response.text().then(function (t) {
+				if (response.status !== 200) {
+					console.log('error');
+
+					return;
+				}
+				alert('API Response: ' + JSON.stringify(JSON.parse(t)));
+			}).catch(function (err) {
+				alert('error: ' + err);
+			});
+		});
+
+
+		//	fetch('http://www.balticapp.fi/lukeA/login', {
+		//		method: 'get',
+		//		headers: new Headers({
+		//			'Content-Type': 'application/json',
+		//			'Authorization': 'Bearer ' + idToken,
+		//			'acstoken': accessToken
+		//		})
+		//	}).then(function (response) {
+		//		response.text().then(function (t) {
+		//			if (response.status !== 200) {
+		//				console.log('error');
+		//				return;
+		//			}
+		//			alert('API Response: ' + JSON.stringify(JSON.parse(t)));
+		//		})
+		//	}).catch(function (err) {
+		//		alert('error: ' + err);
+		//	});
+
+		//retrieve the profile:
+		var retrieve_profile = function () {
+			var id_token = localStorage.getItem('id_token');
+			if (id_token) {
+				lock.getProfile(id_token, function (err, profile) {
+					if (err) {
+						return alert('There was an error getting the profile: ' + err.message);
+					}
+					// Display user information
+					show_profile_info(profile);
+				});
+			}
+		};
+
+		var show_profile_info = function (profile) {
+			$('.nickname').text(profile.nickname);
+			$('.btn-login').hide();
+			$('.avatar').attr('src', profile.picture).show();
+			$('.btn-logout').show();
+		};
+
+
+		var logout = function () {
+			localStorage.removeItem('id_token');
+			window.location.href = "/";
+		};
+
+		retrieve_profile();
+
+	})
 });
