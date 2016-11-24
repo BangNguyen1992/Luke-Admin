1afdba93f191274c99b608f06b3b7efb48a51308
Bang Nguyen <YOUR EMAIL ADDRESS>
2016-11-24 11:59:22 +0200

    Create the exp pattern

--- app.js
+++ app.js
@@ -142,5 +142,37 @@ $(document).ready(function () {
 
 		retrieve_profile();
 
+		$('#submit').click(function () {
+			var title = document.getElementById("title").value;
+			var reportGain = document.getElementById("reportGain").value;
+			var upvoteGain = document.getElementById("upvoteGain").value;
+			var downvoteGain = document.getElementById("downvoteGain").value;
+			var active = document.getElementById("active").value;
+			var JSONObject = {
+				"title": title,
+				"reportGain": reportGain,
+				"upvoteGain": upvoteGain,
+				"downvoteGain": downvoteGain,
+				"active": active,
+			};
+
+			$.ajax({
+				url: 'http://www.balticapp.fi/lukeA/experience/create',
+				type: 'post',
+				data: JSONObject,
+				dataType: 'JSON',
+				success: function (data) {
+					console.log(data);
+					var jsonData = $.parseJSON(data); //if data is not json
+					console.log(jsonData);
+					$('#title').val(jsonData.title);
+					$('#reportGain').val(jsonData.reportGain);
+					$('#upvoteGain').val(jsonData.upvoteGain);
+					$('#downvoteGain').val(jsonData.downvoteGain);
+					$('#active').val(jsonData.active);
+				}
+			});
+		});
+
 	})
 });
